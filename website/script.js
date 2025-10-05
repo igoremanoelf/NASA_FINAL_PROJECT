4933c83ff98873c5f3fad1630e78e54edefc464c
igoremanoelf <igoremanoelfo@gmail.com>
2025-10-05 20:12:24 -0300

    Conecta frontend com a API de produção

--- website/script.js
+++ website/script.js
@@ -260,7 +260,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/predict', {
+            const response = await fetch('https://nasa-api-qljw.onrender.com/predict', {
                 method: 'POST', headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(apiPayload)
             });
